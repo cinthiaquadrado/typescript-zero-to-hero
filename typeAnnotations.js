305b2f1ef50aba03e6c50bfa51d404d6f167b67c
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-18 10:01:42 -0300

    Arquivo typeAnnotations.js

---
+++ typeAnnotations.js
@@ -0,0 +1,19 @@
+//Variaveis [Type Annotations]
+
+let nome = 'Cinthia Yumi';
+console.log(nome);
+
+//Arrays [Type Annotations]
+let animais = ['Elefante', 'Gato', 'Cachorro', 'Panda'];
+console.log(animais);
+
+//Objetos [Type Annotations]
+let carro;
+carro = { nome: 'Toyota', ano: 2019, preco: 80000 };
+console.log(carro);
+
+//Função [Type Annotations]
+function multiplicarNumeros(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicarNumeros(2, 3));
