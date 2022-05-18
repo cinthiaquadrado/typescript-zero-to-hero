d99842b7ddc31d81708fd030d2e2f372ecf2b8c3
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-18 11:42:13 -0300

    Arquivo stringType.js

---
+++ stringType.js
@@ -0,0 +1,25 @@
+//Exemplos
+//Usando aspas simples
+let animal1 = `Panda`;
+console.log(animal1);
+
+let nomeCompleto = 'Cinthia Yumi';
+console.log(nomeCompleto);
+
+//Usando aspas duplas
+let animal2 = "Elefante";
+console.log(animal2);
+
+let funcaoEmpresa = "Desenvolvedora júnior";
+console.log(funcaoEmpresa);
+
+let nomePessoa = 'Cinthia Yumi';
+let nomeEmpresa = 'Microsoft';
+
+//Usando back ticks
+
+let detalhesPessoa = `${nomePessoa} trabalha na empresa ${nomeEmpresa}.`;
+console.log('Detalhes ' + detalhesPessoa);
+
+let dadosFuncionario = `Seja bem-vinda ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}.`;
+console.log(dadosFuncionario);
