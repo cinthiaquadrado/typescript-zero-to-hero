1e51a37575d428a4276eb8afe3304a3f7a137af8
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-18 10:22:12 -0300

    Arquivo para praticar boolean type

---
+++ booleanType.ts
@@ -0,0 +1,34 @@
+//Aula de exercícios
+//Exemplo 01
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+//Exemplo 02
+let concluido: boolean = false;
+
+if(!concluido) {
+    console.log('Tarefa foi concluída com sucesso')
+} else {
+    console.log('Tarefa pendente');
+}
+
+//Exemplo 03
+let maiorIdade: boolean = true;
+
+if(maiorIdade) {
+    console.log('Pode dirigir')
+} else {
+    console.log('Ainda não pode dirigir');
+}
+
+//Exemplo 04
+let estudante: boolean = true;
+
+if(!estudante) {
+    console.log('Vai trabalhar')
+} else {
+    console.log('Precisa estudar');
+}
