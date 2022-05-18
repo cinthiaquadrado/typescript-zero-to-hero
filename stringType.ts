6c3ea9d435ee747b0fa800563e58bcbf9f2c1a9c
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-18 11:34:09 -0300

    Exercícios de string type

---
+++ stringType.ts
@@ -0,0 +1,25 @@
+//Exemplos
+
+//Usando aspas simples
+let animal1: string = `Panda`;
+console.log(animal1);
+
+let nomeCompleto: string = 'Cinthia Yumi'
+console.log(nomeCompleto);
+
+//Usando aspas duplas
+let animal2: string = "Elefante";
+console.log(animal2);
+
+let funcaoEmpresa: string = "Desenvolvedora júnior";
+console.log(funcaoEmpresa);
+
+let nomePessoa: string = 'Cinthia Yumi';
+let nomeEmpresa: string = 'Microsoft';
+
+//Usando back ticks
+let detalhesPessoa: string = `${nomePessoa} trabalha na empresa ${nomeEmpresa}.`;
+console.log('Detalhes ' + detalhesPessoa);
+
+let dadosFuncionario: string = `Seja bem-vinda ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}.`;
+console.log(dadosFuncionario);
