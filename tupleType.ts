562d8e015b9ca26c5229770a50d3a1c9a96a458a
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-05-24 15:39:01 -0300

    Adicionando novos exercícios

--- tupleType.ts
+++ tupleType.ts
@@ -17,3 +17,31 @@ console.log(... listaFrutas);
 //Outra forma de usar tuplas em TS (com labels)
 let pessoa2: [nome: string, posicao: string, idade: number] = ['Cinthia', 'desenvolvedora', 29]
 console.log(pessoa2);
+
+let listaFrutas: [string, ...string[]] = ['Abacaxi', 'Maçã', 'Laranja', 'Melancia', 'Morango'];
+console.log(... listaFrutas);
+
+//Lista heterogênea de tupla
+let listaFrutas2: [number, boolean, ...string[]] = [5, true, ...listaFrutas];
+console.log(listaFrutas2);
+
+//Uso da função com tuplas
+function listarPessoas(nomes: string[], idades: number[]) {
+    return [...nomes, ...idades];
+}
+
+let resultado = listarPessoas(['Cinthia', 'Vinicius'],[29, 30])
+console.log(resultado);
+
+//Labeled Tuples com spread operator numa função
+
+type Nome = 
+    | [primeiroNome: string, sobrenome: string]
+    | [primeiroNome: string, nomeMeio: string, sobrenome: string]
+
+function criarPessoa (...nome: Nome) {
+    return [... nome];
+}
+
+console.log(criarPessoa('Cinthia', 'Quadrado'))
+console.log(criarPessoa('Cinthia', 'Hirakawa', 'Quadrado'))
